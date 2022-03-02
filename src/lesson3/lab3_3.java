89b805ca6878647ce46aa3ab1bbbf3d0c920c6f6
trishtr <trantrang.ip@gmail.com>
2022-03-01 17:45:53 -0800

    sort Arrays using for loop

--- src/lesson3/lab3_3.java
+++ src/lesson3/lab3_3.java
@@ -6,11 +6,29 @@ public class lab3_3 {
 
     public static void main(String[] args) {
 
-        int [] myArray = {12,34,1,16,28};
-        Arrays.sort(myArray);
+        int[] myArray = {12, 34, 1, 16, 28};
+        // Arrays.sort(myArray);
+        int length = myArray.length;
+        for(int i= 0; i< length; i ++)
+        {
+            for (int j = i; j < length; j++)
+            {
+                if(myArray[i] > myArray[j])
+                {
+                    int temp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = temp;
+                }
+            }
+        }
 
-       for (int i = 0; i< myArray.length; i ++){
-            System.out.print(myArray[i] + " ");
-       }
+        for(int k = 0; k< length; k++)
+        {
+            System.out.print(myArray[k] + " ");
+        }
+
+
     }
-}
+
+    }
+
